aa5ce4a7d489a99f7318520106b64b16ccab8351
Rémi Vedrenne <remi.vedrenne@dualcat.io>
2023-02-13 13:09:02 +0100

    [CODE] Create burst animation class and move animation to the trigger data

--- Assets/Script/Spell/SpellFx/SpellAnimation.cs
+++ Assets/Script/Spell/SpellFx/SpellAnimation.cs
@@ -5,11 +5,7 @@ public abstract class SpellAnimation : MonoBehaviour
 {
     public float BaseSpellDelay => GetAnimationDuration();
 
-    private void Awake()
-    {
-        Animate();
-        DestroySelf(GetAnimationDuration());
-    }
+    
 
     public void TriggerFx(Vector3 position,Transform targetEntity = null)
     {
@@ -26,4 +22,24 @@ public abstract class SpellAnimation : MonoBehaviour
     protected abstract float GetAnimationDuration();
     public abstract void Animate();
     public abstract void DestroySelf(float time);
+}
+
+public class BurstAnimation : SpellAnimation
+{
+    private void Awake()
+    {
+        Animate();
+        DestroySelf(GetAnimationDuration());
+    }
+    protected override float GetAnimationDuration()
+    {
+        return 0;
+    }
+
+    public override void Animate() {}
+
+    public override void DestroySelf(float time)
+    {
+        Destroy(gameObject,time);
+    }
 }
