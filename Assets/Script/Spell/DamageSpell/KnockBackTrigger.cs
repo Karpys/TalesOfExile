64efa9d2997dd4cfa0bee918ff4a2ddaa0969e72
Karpys <remi.vedrenne.pro@gmail.com>
2023-03-06 00:32:34 +0100

    [CODE] Add string logic additional parameters to parameters less

---
+++ Assets/Script/Spell/DamageSpell/KnockBackTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using TweenCustom;
+using UnityEngine;
+
+public class KnockBackTrigger : DamageSpellTrigger
+{
+    private List<BoardEntity> m_EntityHits = new List<BoardEntity>();
+    public KnockBackTrigger(DamageSpellScriptable damageSpellData) : base(damageSpellData)
+    {}
+
+    public override void Trigger(TriggerSpellData spellData, SpellTiles spellTiles)
+    {
+        m_EntityHits.Clear();
+        base.Trigger(spellData, spellTiles);
+    }
+
+    protected override void EntityHit(BoardEntity entity, TriggerSpellData spellData, EntityGroup targetGroup,Vector2Int origin)
+    {
+        if(m_EntityHits.Contains(entity))
+            return;
+        
+        m_EntityHits.Add(entity);
+        Vector2Int opposite = TileHelper.GetOppositePositionFrom(entity.EntityPosition, spellData.AttachedEntity.EntityPosition);
+        
+        if (MapData.Instance.IsWalkable(opposite))
+        {
+            entity.MoveTo(opposite,false);
+            entity.transform.DoMove(MapData.Instance.GetTilePosition(entity.EntityPosition),0.1f);
+            m_SpellAnimDelay = 0.1f;
+        }
+        else
+        {
+            base.EntityHit(entity, spellData, targetGroup,origin);
+        }
+    }
+}
---
+++ Assets/Script/Utils/StringUtils.cs
@@ -0,0 +1,7 @@
+﻿namespace Script.Utils
+{
+    public class StringUtils
+    {
+        
+    }
+}
