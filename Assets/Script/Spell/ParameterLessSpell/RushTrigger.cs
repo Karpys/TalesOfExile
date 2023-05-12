2d556c8ad9d361fe18d8da636ba9991adb293885
Karpys <remi.vedrenne.pro@gmail.com>
2023-05-12 21:13:57 +0200

    [BALANCE] Reduce all damage / life / auto attack force

--- Assets/Script/Spell/ParameterLessSpell/RushTrigger.cs
+++ Assets/Script/Spell/ParameterLessSpell/RushTrigger.cs
@@ -1,26 +1,16 @@
 ﻿using UnityEngine;
 
-public class RushTrigger : DamageSpellTrigger
+public class RushTrigger : WeaponDamageTrigger
 {
-    public RushTrigger(DamageSpellScriptable damageSpellData) : base(damageSpellData)
+    public RushTrigger(DamageSpellScriptable damageSpellData, float baseWeaponDamageConvertion) : base(damageSpellData, baseWeaponDamageConvertion)
     {
     }
-
     public override void Trigger(TriggerSpellData spellData, SpellTiles spellTiles,CastInfo castInfo)
     {
         MoveToClosestFreeTile(spellData,spellTiles.OriginTiles[0]);
         base.Trigger(spellData, spellTiles,castInfo);
     }
-
-    protected override void EntityHit(BoardEntity entity, TriggerSpellData spellData, EntityGroup targetGroup,
-        Vector2Int origin, CastInfo castInfo)
-    {
-        base.EntityHit(entity, spellData, targetGroup, origin,castInfo);
-        //Silence Showcase
-        //Buff silenceDebuff = BuffLibrary.Instance.GetBuffViaKey(BuffType.SilenceDebuff, entity);
-        //silenceDebuff.InitializeBuff(spellData.AttachedEntity,entity,10,0);
-    }
-
+    
     private void MoveToClosestFreeTile(TriggerSpellData spellData,Vector2Int position)
     {
         Tile closestFree = TileHelper.GetFreeClosestAround(MapData.Instance.GetTile(position),spellData.AttachedEntity.WorldPosition);
