c558f5e58e9ba33ece10cf16d463946fe3910db3
Rémi Vedrenne <remi.vedrenne@dualcat.io>
2023-05-22 11:48:55 +0200

    [UI] Start spell selection ui

---
+++ Assets/Script/UI/SpellSelectionUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SpellSelectionUI:MonoBehaviour
+{
+    [SerializeField] private Transform m_LayoutTransform = null;
+
+    private const float BASE_HEIGHT = 4f;
+    private const float BASE_WIDTH = 100f;
+}
