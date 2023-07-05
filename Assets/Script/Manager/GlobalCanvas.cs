a9fbc7a757e68ae633a2c5a2243f9ee6075eb215
Karpys <remi.vedrenne.pro@gmail.com>
2023-07-05 20:42:06 +0200

    [CODE] Add reference size and width ratio ui clamp

--- Assets/Script/Manager/GlobalCanvas.cs
+++ Assets/Script/Manager/GlobalCanvas.cs
@@ -6,6 +6,7 @@ namespace KarpysDev.Script.Manager
 {
     public class GlobalCanvas : SingletonMonoBehavior<GlobalCanvas>
     {
+        [SerializeField] private Vector2 m_ReferenceSize = Vector2.zero;
         private Vector2 m_ScreenSize = Vector2.zero;
         private UIPointer m_CurrentPointer = null;
         private UICanvasType m_CurrentCanvasType = UICanvasType.None;
@@ -14,9 +15,12 @@ namespace KarpysDev.Script.Manager
         public UICanvasType CanvasType => m_CurrentCanvasType;
         public Vector2 ScreenSize => m_ScreenSize;
 
+        private float m_WidthRatio = 0;
+
         private void Start()
         {
             m_ScreenSize = new Vector2(Screen.width,Screen.height);
+            m_WidthRatio = m_ScreenSize.x / m_ReferenceSize.x;
         }
 
         public void SetCanvasPointer(UIPointer pointer, UICanvasType canvasType)
@@ -40,7 +44,7 @@ namespace KarpysDev.Script.Manager
             float uiSize = uiTransform.rect.width;
             float posX = uiTransform.position.x;
             
-            posX = Mathf.Clamp(posX, uiSize / 2, Screen.width - uiSize / 2);
+            posX = Mathf.Clamp(posX, (uiSize / 2 ) * m_WidthRatio, Screen.width - uiSize / 2*m_WidthRatio);
             Transform trans = uiTransform.transform;
             trans.position = new Vector3(posX, trans.position.y);
         }
