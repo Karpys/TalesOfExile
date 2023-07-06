d81389f72f2d925227a09d9bcbd39fa63dd8bedb
Karpys <remi.vedrenne.pro@gmail.com>
2023-07-06 14:56:12 +0200

    [UI/CODE] Add debuff ui

--- Assets/Script/UI/BuffUIController.cs
+++ Assets/Script/UI/BuffUIController.cs
@@ -4,16 +4,22 @@ using KarpysDev.Script.Entities;
 using KarpysDev.Script.Entities.BuffRelated;
 using KarpysDev.Script.Manager;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace KarpysDev.Script.UI
 {
     public class BuffUIController : MonoBehaviour
     {
+        [SerializeField] private HorizontalLayoutGroup m_Layout = null;
+        [SerializeField] private float m_LayoutSpacing = 0;
         [SerializeField] private Transform m_BuffContainer = null;
         [SerializeField] private BuffUI m_BuffUIPrefab = null;
+        [SerializeField] private Transform m_DebuffContainer = null;
+        [SerializeField] private BuffUI m_DebuffUIPrefab = null;
         [SerializeField] private BuffUIDisplayer m_Displayer = null;
         
         private List<BuffUI> m_BuffUIs = new List<BuffUI>();
+        private int m_BuffCount = 0;
         private void Start()
         {
             GameManager.Instance.A_OnControlledEntityChange += OnControlledEntityChange;
@@ -64,16 +70,40 @@ namespace KarpysDev.Script.UI
                 Destroy(m_BuffUIs[i].gameObject);
             }
             m_BuffUIs.Clear();
+            m_BuffCount = 0;
         }
 
         private void CreateBuffUI(Buff buff)
         {
             if(buff.BuffCooldown != BuffCooldown.Cooldown)
                 return;
-        
-            BuffUI buffUI = Instantiate(m_BuffUIPrefab, m_BuffContainer);
+
+            BuffUI buffUI = null;
+            
+            if (buff.BuffGroup == BuffGroup.Buff)
+            {
+                m_BuffCount += 1;
+                buffUI = Instantiate(m_BuffUIPrefab, m_BuffContainer);
+            }
+            else
+            {
+                buffUI = Instantiate(m_DebuffUIPrefab, m_DebuffContainer);   
+            }
             buffUI.Initialize(buff,m_Displayer);
             m_BuffUIs.Add(buffUI);
+            UpdateLayoutSpacing();
+        }
+
+        private void UpdateLayoutSpacing()
+        {
+            if (m_BuffCount == 0)
+            {
+                m_Layout.spacing = 0;
+            }
+            else
+            {
+                m_Layout.spacing = m_LayoutSpacing;
+            }
         }
     
         private void RemoveBuffUI(Buff buff)
@@ -84,7 +114,13 @@ namespace KarpysDev.Script.UI
             {
                 m_BuffUIs.Remove(targetBuffUI);
                 Destroy(targetBuffUI.gameObject);
+
+                if (targetBuffUI.AttachedBuff.BuffGroup == BuffGroup.Buff)
+                {
+                    m_BuffCount -= 1;
+                }
             }
+            UpdateLayoutSpacing();
         }
     
         private void OnAddBuff(Buff buff)
