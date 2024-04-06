5896065430a8aba0613030cdb93072378c2d90cb
Karpys <remi.vedrenne.pro@gmail.com>
2024-04-06 21:26:27 +0200

    [CODE] Add psionic hit system

---
+++ Assets/Script/Entities/BuffRelated/PsionicHitBuff.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KarpysDev.Script.Manager;
+using KarpysDev.Script.Spell;
+using KarpysDev.Script.Utils;
+using UnityEngine;
+
+namespace KarpysDev.Script.Entities.BuffRelated
+{
+    public class PsionicHitBuff : Buff
+    {
+        private int m_HitCount = 0;
+        private Zone m_Zone = null;
+        private SpellInfo m_SpellInfo = null;
+        private TriggerSpellData m_TriggerSpellData = null;
+        private List<Vector2Int> m_HitReceivers = new List<Vector2Int>();
+        
+        public PsionicHitBuff(BoardEntity caster, BoardEntity receiver, BuffType buffType, BuffGroup buffGroup, int cooldown, float buffValue,SpellInfo spellInfo,int hitCount,ZoneType zoneType,int range) : base(caster, receiver, buffType, buffGroup, cooldown, buffValue)
+        {
+            m_SpellInfo = spellInfo;
+            m_Zone = new Zone(zoneType, range);
+            m_HitCount = hitCount;
+        }
+
+        public override void Apply()
+        {
+            m_TriggerSpellData = m_Receiver.RegisterSpell(m_SpellInfo);
+            m_TriggerSpellData.SpellTrigger.ComputeSpellData(m_Receiver);
+            
+            m_Receiver.EntityEvent.OnRequestCastEvent += AddPsionicHit;
+            m_Receiver.ComputeAllSpells();
+            m_Receiver.EntityEvent.OnSpellRecompute += Recompute;
+        }
+
+        private void AddPsionicHit(BaseSpellTrigger spellTrigger)
+        {
+            if (spellTrigger.SpellData.Data.SpellGroups.Contains(SpellGroup.UnarmedHit))
+            {
+                spellTrigger.OnCastSpell += PsionicHitCallBack;
+            }
+        }
+
+        private void PsionicHitCallBack(CastInfo castInfo)
+        {
+            if (!(castInfo is DamageCastInfo damageCastInfo)) return;
+
+            if (GameManager.Instance.AddCallBackAction(PsionicHit))
+            {
+                if (damageCastInfo.HitEntity.Count > 0)
+                {
+                    List<BoardEntity> allEntity = new List<BoardEntity>(GameManager.Instance.GetEntityViaGroup(m_Receiver.TargetEntityGroup));
+                    m_HitReceivers.AddRange(DistanceUtils.GetContactAround(m_Zone, allEntity, damageCastInfo.HitEntity[0].EntityPosition, m_HitCount,true));
+                }
+            }
+        }
+
+        private void PsionicHit()
+        {
+            foreach (Vector2Int hitReceiver in m_HitReceivers)
+            {
+                SpellCastUtils.TriggerSpellAt(m_TriggerSpellData, hitReceiver,m_Receiver.EntityPosition);
+            }
+            m_HitReceivers.Clear();
+        }
+
+        protected override void UnApply()
+        {
+            m_Receiver.EntityEvent.OnRequestCastEvent -= AddPsionicHit;
+            m_Receiver.ComputeAllSpells();
+            m_Receiver.EntityEvent.OnSpellRecompute -= Recompute;
+        }
+        
+        private void Recompute()
+        {
+            m_TriggerSpellData.SpellTrigger.ComputeSpellData(m_Receiver);
+        }
+    }
+}
---
+++ Assets/Script/Spell/ParameterLessSpell/PsionicBuffGiver.cs
@@ -0,0 +1,26 @@
+﻿using KarpysDev.Script.Entities;
+using KarpysDev.Script.Entities.BuffRelated;
+using KarpysDev.Script.Manager.Library;
+
+namespace KarpysDev.Script.Spell.ParameterLessSpell
+{
+    public class PsionicBuffGiver : BuffGiverTrigger
+    {
+        private int m_HitCount = 0;
+        private ZoneType m_ZoneType = ZoneType.Circle;
+        private int m_Range = 0;
+        public PsionicBuffGiver(BaseSpellTriggerScriptable baseScriptable, BuffGroup buffGroup, BuffType buffType, BuffCooldown buffCooldown, int buffDuration, float buffValue, VisualEffectType visualEffectType,int hitCount,ZoneType zoneType,int range) : base(baseScriptable, buffGroup, buffType, buffCooldown, buffDuration, buffValue, visualEffectType)
+        {
+            m_HitCount = hitCount;
+            m_ZoneType = zoneType;
+            m_Range = range;
+        }
+
+        protected override Buff BuffToAdd(BoardEntity caster, BoardEntity receiver)
+        {
+            return new PsionicHitBuff(caster, receiver, m_BuffType, m_BuffGroup, m_BuffDuration, m_BuffValue,
+                BuffLibrary.Instance.UnarmedPsionicHitSpellInfo
+                , m_HitCount, m_ZoneType, m_Range);
+        }
+    }
+}
