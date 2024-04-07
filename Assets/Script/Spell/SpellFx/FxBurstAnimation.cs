6bac5b9ff5e5aa3151f6fba77245a098a2e24ebf
Rémi Vedrenne <remi.vedrenne.pro@gmail.com>
2024-04-07 23:03:51 +0200

    [ART/CODE] Add palm clap sprite and submodule update

--- Assets/Script/Spell/SpellFx/FxBurstAnimation.cs
+++ Assets/Script/Spell/SpellFx/FxBurstAnimation.cs
@@ -1,10 +1,28 @@
 namespace KarpysDev.Script.Spell.SpellFx
 {
+    using UnityEngine;
+
     public abstract class FxBurstAnimation : SpellAnimation
     {
         protected virtual void Start()
         {
             Animate();
+        }
+    }
+
+    public class FxPalmClap : FxBurstAnimation,ISizeable
+    {
+        [SerializeField] private Transform m_RightPalm = null;
+        [SerializeField] private Transform m_LeftPalm = null;
+        private int m_Size = 0;
+        protected override void Animate()
+        {
+            
+        }
+
+        public void SetSize(int size)
+        {
+            m_Size = size;
         }
     }
 }
