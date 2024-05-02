ca98ef7152782052c0db9a32df953412e51e9396
Karpys <remi.vedrenne.pro@gmail.com>
2024-05-02 13:14:33 +0200

    [CODE/Project] Add asmdef and update spell fielder assets

--- Assets/Script/Widget/ModifierEquipementPoolScriptable.cs
+++ Assets/Script/Widget/ModifierEquipementPoolScriptable.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 namespace KarpysDev.Script.Widget
 {
     using KarpysUtils;
-
-    [CreateAssetMenu(fileName = "ModifierEquipementPool", menuName = "Modifier/ModifierEquipementPool", order = 0)]
+    //Obsolete
+    //[CreateAssetMenu(fileName = "ModifierEquipementPool", menuName = "Modifier/ModifierEquipementPool", order = 0)]
     public class ModifierEquipementPoolScriptable : ScriptableObject
     {
         public GenericLibrary<ModifierPoolScriptable, EquipementType> m_EquipementModifierPool = null;
